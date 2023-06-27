5c44e0e6d5a5c8ba220fa6a8b849486e49c8d775
LipeiDu <lpdu@gmail.com>
2023-06-27 14:21:14 -0400

    generate jobs scripts

---
+++ config/parameters_dict_master.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+"""
+    This script contains all the default parameters in the iEBE-MUSIC package.
+"""
+
+from os import path, makedirs
+import sys
+import shutil
+import argparse
+
+# control parameters
+control_dict = {
+    'walltime': "10:00:00",  # walltime to run
+}
+
+dilepton_dict = {
+    'hydro_flag': 2,                # read in mode for hydro medium
+                                  # 0: read in hdf5 file
+                                  # 1: read in binary file output from MUSIC
+                                  # 2: read in binary file output from new MUSIC (no grid)
+                                  # 3: read in binary file output from new MUSIC (on grid)
+    'hydro_nskip_tau': 1,           # read in hydro slice every hydro_nskip_tau 
+                                  # steps from the medium file
+                                  # (only works for hydro_flag = 1)
+
+
+    ##### emission rate #####
+
+    'dilepton_emission_rate': 2,    # 0: analytical emission rate; PRC. 93, 044902, 2016
+                                  # 1: LO rate; (2.4) of 1910.09567
+                                  # 2: resummed NLO; from GJ, extrapolation will NOT work properly for large alpha_s, large muB, 
+                                  # or if q falls outside the grid! In these cases, just the boundary value is used.
+
+    'alpha_s': 0.0,                 # strong coupling, works for dilepton_emission_rate = 2
+
+
+    ##### hydro profile #####
+
+    'Xmin': -15.0,                  # minimum points along x direction
+    'dx': 0.3,                      # lattice spacing along x direction
+    'Ymin': -15.0,                  # minimum points along y direction
+    'dy': 0.3,                      # lattice spacing along y direction
+
+    'neta': 10,                     # number of points in eta direction
+    'eta_i': 0.0,                   # beginning value of eta slice
+    'eta_f': 3.0,                   # end value of eta slice
+
+    'T_sw_high': 0.140,             # high end of the switching temperature, above which is full QGP
+    'T_sw_low': 0.135,              # low end of the switching temperature
+    'T_dec': 0.105,                 # freeze out temperature (GeV), below which is out of interest
+
+
+    ##### hydro settings #####
+
+    'HydroinfoVisflag': 1,          # determine whether to read in the viscous evolution information
+    'HydroinfoBuffersize': 500,     # set the buffer size for hydro evolution profile
+
+    'include_baryondiff_deltaf': 0, # switch to include baryon diffusion corrections
+    'include_shearvisc_deltaf': 0,  # switch to include shear viscous corrections
+    'turn_off_transverse_flow': 0,  # flag to turn off transverse flow in the photon calculation
+    'turn_on_muB': 1,               # flag to include muB dependence in photon rates
+
+    'test_code_flag': 1,            # flag to test the code with constant T, muB etc.
+    'T_test': 0.2,
+    'muB_test': 0.3,
+    'rhoB_eplusp_test': 2.0,
+    'inv_eplusp_test': 1.0,
+
+
+    ##### photon kinematics #####
+
+    'np': 20,                       # number of points for photon momentum
+    'nphi': 40,                     # number of points for angles of photons momenta
+    'nrapidity': 1,                 # number of points for photon rapidity, odd number
+    'nm': 3,                       # number of points for dilepton invariant mass
+
+    'photon_q_i': 0.001,              # the smallest photon momentum to be calculated
+    'photon_q_f': 4.0,              # the largest photon momentum to be calculated
+    'photon_phi_q_i': 0.0,          # the smallest angle of photon momentum
+    'photon_phi_q_f': 6.2831853,    # the largest angle of photon momentum
+    'photon_y_i': 0.0,              # the smallest photon rapidity
+    'photon_y_f': 0.0,              # the largest photon rapidity
+    'dilepton_mass_i': 0.1,         # the smallest dilepton invariant mass
+    'dilepton_mass_f': 3.6,         # the largest dilepton invariant mass
+
+    'norder': 3,                    # calculate photon vn to norder
+
+    'differential_flag': 0,        # determine whether to output differential photon yield and vn
+                                 # 1: differential in T and tau
+                                 # 2: differential in x and tau
+                                 # 10: differeitial in all options above
+    'tau_start': 3.0,              # emission start time (fm)
+    'tau_end': 13.0,               # emission end time (fm)
+    'n_tau_cut': 15,               # number of points in tau (range of tau is specified by tau_start and tau_end)
+    'T_cuthigh': 0.25,             # maximum allowed emission T (GeV)
+    'T_cutlow': 0.1,              # minimum allowed emission T (GeV)
+    'nTcut': 10,                   # number of points in T (range of T is specified by T_cuthigh and T_cutlow)
+    'dTau': 0.1,                   # lattice spacing along tau direction
+
+    'calHGIdFlag': 0,               # Flag to decide whether to calculate individual HG channels
+
+}
+
+Parameters_list = [
+    (dilepton_dict, "parameters.dat", 1)
+]
+
+path_list = [
+    'model_parameters/'
+]
+
+
+def update_parameters_dict(par_dict_path):
+    """This function update the parameters dictionaries with user's settings"""
+    par_diretory = path.dirname(par_dict_path)
+    sys.path.insert(0, par_diretory)
+    print("\U0001F375  User's parameter file found in {}".format(par_diretory))
+
+    parameters_dict = __import__(par_dict_path.split('.py')[0].split('/')[-1])
+    
+    dilepton_dict.update(parameters_dict.dilepton_dict)
+
+
+def output_parameters_to_files(workfolder="."):
+    """This function outputs parameters in dictionaries to files"""
+    workfolder = path.abspath(workfolder)
+    
+    for idict, (parameters_dict, fname, itype) in enumerate(Parameters_list):
+        #output_folder = path.join(workfolder, path_list[idict])
+        output_folder = path.join(workfolder)
+        if not path.exists(output_folder):
+            makedirs(output_folder)
+        f = open(path.join(output_folder, fname), "w")
+        for key_name in parameters_dict:
+            if itype == 1:
+                f.write("{parameter_name} = {parameter_value}\n".format(
+                    parameter_name=key_name,
+                    parameter_value=parameters_dict[key_name]))
+        f.close()
+
+        print("\U0001F375  Output input parameter files to {}...".format(
+                                                                output_folder))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description='\U0000269B Welcome to dilepton parameter master',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-path', '--path', metavar='',
+                        type=str, default='.',
+                        help='output folder path')
+    parser.add_argument('-par', '--par_dict', metavar='',
+                        type=str, default='parameters_dict_user',
+                        help='user-defined parameter dictionary filename')
+
+    args = parser.parse_args()
+
+    update_parameters_dict(path.abspath(args.par_dict))
+    output_parameters_to_files(args.path)
+
+
---
+++ generate_jobs.py
@@ -0,0 +1,125 @@
+import os
+import sys
+from os import path, makedirs
+import shutil
+import subprocess
+
+
+def generate_submit_jobs(script_dir, directory, walltime):
+
+    working_folder = os.path.join(script_dir, directory)
+    submit_jobs_path = os.path.join(script_dir, directory, "submit_jobs.pbs")
+    # Generate the submit_jobs.pbs script content
+    script_content = """#!/usr/bin/env bash
+#SBATCH -J dilepton_event_0
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=15G
+#SBATCH -e test.err
+#SBATCH -o test.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lipei.du@mail.mcgill.ca
+#SBATCH --account=def-gale
+#SBATCH -t {1:s}
+#SBARCH -D {0:s}
+
+module load nixpkgs/16.09  intel/2018.3  impi/2018.3.222
+module load gsl
+module load hdf5-mpi/1.8.18
+module load StdEnv/2020 intel/2020.1.217 hdf5/1.12.1
+module load gcc
+
+# Run the dilepton_emission.e program
+./dilepton_emission.e
+    """.format(working_folder, walltime)
+
+    # Write the script content to a file
+    with open('submit_jobs.pbs', 'w') as script_file:
+        script_file.write(script_content)
+
+def create_directory(folder_name, beam_energy, centrality_low, centrality_high):
+    # Create directory name
+    dir_name = f"{folder_name}{beam_energy}_{centrality_low}_{centrality_high}"
+
+    # Check if the directory already exists
+    if os.path.exists(dir_name):
+        print(f"Warning: Directory '{dir_name}' already exists.")
+
+        # Prompt user for action
+        user_input = input("Do you want to delete the existing directory? (y/n): ")
+
+        if user_input.lower() == 'y':
+            # Delete the existing directory
+            shutil.rmtree(dir_name)
+        else:
+            print("Exiting the program.")
+            return None
+
+    # Create the directory
+    os.makedirs(dir_name, exist_ok=True)
+
+    # Change to the new directory
+    os.chdir(dir_name)
+
+    # Create the results directory
+    os.makedirs('results', exist_ok=True)
+    #os.makedirs('model_parameters', exist_ok=True)
+
+    # Return the created directory name
+    return dir_name
+
+
+if __name__ == '__main__':
+    # Check if all required arguments are provided
+    if len(sys.argv) != 6:
+        print("Usage: python script.py folder_name beam_energy centrality_low centrality_high")
+        sys.exit(1)
+
+    # Retrieve the command line arguments
+    folder_name = sys.argv[1]
+    beam_energy = sys.argv[2]
+    centrality_low = sys.argv[3]
+    centrality_high = sys.argv[4]
+    par_dict = sys.argv[5] # parameters_dict_user.py
+
+    par_diretory = path.dirname(path.abspath(par_dict))
+    sys.path.insert(0, par_diretory)
+    parameter_dict = __import__(par_dict.split('.py')[0].split("/")[-1])
+
+    # Retrieve the absolute directory path of the script, dilepton/
+    script_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # Retrieve the parent directory path, lpdu/
+    parent_directory = os.path.dirname(script_dir)
+
+    # Call the function to create the directory structure and get the directory name, e.g. AuAu_19.6_0_10
+    dir_name = create_directory(folder_name, beam_energy, centrality_low, centrality_high)
+
+    event_dir = os.path.join(script_dir, dir_name)
+
+    # copy parameter python files
+    par_dict_path = os.path.join(script_dir, par_dict)
+    dir_path = os.path.join(script_dir, dir_name)
+    shutil.copy2(par_dict_path, dir_path)
+
+    subprocess.call("(cd {}/config; ".format(script_dir)  + "python3 parameters_dict_master.py "
+        + "-path {} -par {};)".format(dir_path, path.abspath(par_dict)), shell=True)
+
+    # Create symbolic links
+    os.symlink('{}/dilepton_emission.e'.format(script_dir), '{}/dilepton_emission.e'.format(event_dir))
+    #os.symlink('{}/parameters.dat'.format(script_dir), '{}/parameters.dat'.format(event_dir))
+    os.symlink('{}/ph_rates'.format(script_dir), '{}/ph_rates'.format(event_dir))
+
+    os.symlink("{0:s}/iEBE-sampler/{1:s}/event_0/EVENT_RESULTS_MCGlb{1:s}_0/hydro_results_MCGlb{1:s}_0/evolution_all_xyeta.dat".format(parent_directory,dir_name), 
+        "{}/results/evolution_all_xyeta.dat".format(event_dir))
+    os.symlink("{0:s}/iEBE-sampler/{1:s}/event_0/EVENT_RESULTS_MCGlb{1:s}_0/hydro_results_MCGlb{1:s}_0/music_input".format(parent_directory,dir_name), 
+        "{}/results/music_input".format(event_dir))
+
+    if "walltime" in parameter_dict.control_dict.keys():
+        walltime = parameter_dict.control_dict["walltime"]
+
+    # Call the function to generate submit_jobs.pbs inside the created directory
+    generate_submit_jobs(script_dir, dir_name, walltime)
+
+
+
